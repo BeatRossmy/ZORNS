a5fabbe89e405e798f899926e37476ac60a40832
BeatRossmy <39985617+BeatRossmy@users.noreply.github.com>
2022-02-28 22:14:17 +0100

    Add files via upload

--- zorns.lua
+++ zorns.lua
@@ -1,75 +1,85 @@
-Signal = {
-  -- range: [-1,1]
-  new = function (v)
-    return {value=v}
-  end,
-  map = function (s,l,h,q)
-    if type(s)=="table" then s = s.value end
-    local v = util.linlin(-5,5,l,h,s)
-    if q then v = util.round(v,q) end
-    return v
-  end,
-  to_midi = function (s)
-    if type(s)=="table" then s = s.value end
-    local v = util.linlin(-5,5,0,120,s)
-    v = util.round(v,1)
-    return v
-  end,
-  to_level = function (s)
-    if type(s)=="table" then s = s.value end
-    local v = util.linlin(-5,5,0,15,s)
-    v = util.round(v,1)
-    return v
-  end
-}
+include("lib/connections")
 
 include("lib/zorns_module")
 
 include("lib/modules")
 
+select_module = function (x,y)
+  for _,m in pairs(MODULES) do
+    if m:in_area(x,y) then
+      local p = m:get_cell(x,y)
+      local t = m:get_type(p)
+      return SEL.new(x,y,m,p,t)
+    end
+  end
+  return nil
+end
+
+create_connection = function (a,b)
+  if a.type==b.type or a.type=="value" or b.type=="value" then return end
+  local src = a.type=="output" and a or b
+  local trgt = a.type=="input" and a or b
+  local con = CON.new(src,trgt,1)
+  trgt.module:add_connection(trgt.port,con)
+  -- table.insert(CONNECTIONS,con)
+end
 
 g = grid.connect()
 g.key = function(x,y,z)
-  local selected_module = nil
+  -- local selected_module = nil
   
-  for _,n in pairs(nodes) do
-    if n:in_area(x,y) then
-      selected_module = n
-      break
+  -- FIRST SELECTION
+  if not selection and z==1 then
+    selection = select_module(x,y)
+    if selection then
+      selection.module:grid_event(x,y,z)
+    end
+  -- REMOVE SELECTION
+  elseif selection and x==selection.x and y==selection.y and z==0 then
+    selection = nil
+  -- SECOND SELECTION
+  elseif selection and z==1 then
+    local sec_sel = select_module(x,y)
+    if sec_sel then
+      print("create connection")
+      create_connection(selection,sec_sel)
     end
   end
   
-  if selected_module then
-    selected_module:grid_event(x,y,z)
-  else
+  if selection==nil then
     -- empty cell
     if z==1 and not selected_cell then
       selected_cell = {x=x, y=y}
       option = 0
-    else
+    elseif z==0 and selected_cell then
       if option~=0 then
-        table.insert(nodes,modules[option].new(selected_cell.x,selected_cell.y))
+        table.insert(MODULES,modules[option].new(selected_cell.x,selected_cell.y))
       end
       selected_cell = nil
     end
   end
   
 end
+
 m_out = midi.connect(2)
+
+selection = nil -- NEW
 
 selected_port = nil
 selected_cell = nil
 option = 0
 
-nodes = {}
+-- nodes = {}
+MODULES = {}
+-- CONNECTIONS = {}
 CTRL_RATE = 1/128 -- 1/128
 
 ctrl_loop = function ()
-  for _,n in pairs(nodes) do n:ctrl_rate() end
+  for _,m in pairs(MODULES) do m:ctrl_rate() end
 end
 
 draw_loop = function ()
-  for _,n in pairs(nodes) do n:show(g) end
+  for _,m in pairs(MODULES) do m:show(g) end
   g:refresh()
 end
 
@@ -80,11 +90,32 @@ end
 function redraw()
   screen.clear()
   
-  if selected_port then
+  --if selected_port then
+  if selection then
+    local port = SEL.get_port(selection)
+    local port_name = SEL.get_name(selection)
+    local path = selection.module.name.."->"..selection.type.."->"..port_name
+    
     screen.move(16,16)
-    screen.text("type: "..selected_port.module.name)
-    screen.move(32,32)
-    screen.text(string.format(selected_port.name..": %.2f",selected_port.signal.value))
+    screen.text(path)
+    
+    if selection.type=="input" then
+      local y = 48
+      local r = 4
+      screen.move(16,y)
+      screen.line(112,y)
+      screen.stroke()
+      -- SIGNAL
+      local x = util.linlin(0,1,16,112,IN.get(port))
+      screen.move(x,y-r)
+      screen.line(x,y+r)
+      screen.stroke()
+      -- BIAS
+      x = util.linlin(0,1,16,112,port.bias)
+      screen.circle(x,y,r)
+      screen.stroke()
+    end
+    
   elseif not selected_cell then
     screen.move(32,32)
     screen.text("hold empty cell")
@@ -101,7 +132,6 @@ function init ()
   
   metro.init(draw_loop, 1/15):start()
   
-  --metro.init(redraw, 1/15):start()
   clock.run(
     function()
       while true do
@@ -110,38 +140,19 @@ function init ()
       end
     end  
   )
-  
-  --[[lfo_1 = sine_lfo(1,1)
-  
-  lfo_2 = sine_lfo(8,1)
-  lfo_2.inputs[1].value = -0.666
-  
-  quant = quantizer(1,3)
-  
-  m_o = new_output(1,5)
-  
-  lfo_1.outputs[1] = m_o.inputs[1]
-  lfo_2.outputs[1] = quant.inputs[1]
-  quant.outputs[1] = m_o.inputs[2]
-  
-  table.insert(nodes,lfo_1)
-  table.insert(nodes,lfo_2)
-  table.insert(nodes,quant)
-  table.insert(nodes,m_o)--]]
 end
 
 function key (n,z)
-  if n==1 and z==1 and selected_port then
-    selected_port.signal = nil
-    selected_port.module[selected_port.type][selected_port.index] = Signal.new(1)
-    selected_port = nil
+  if n==1 and z==1 and selection then
+    local port = SEL.get_port(selection)
+    port.source = nil
   end
 end
 
 function enc (n,d)
-  if selected_port then
-    selected_port.signal.value = util.clamp(selected_port.signal.value+d*0.1,-5,5)
-    print(selected_port.signal.value)
+  if selection and selection.type=="input" then
+    local port = SEL.get_port(selection)
+    port.bias = util.clamp(port.bias+d*0.01,0,1)
   elseif selected_cell then
     option = util.clamp(option+d,0,#modules)
     print(option)
